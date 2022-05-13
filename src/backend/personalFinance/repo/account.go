3facc12919bfb4156d41ffc97d0b7a38a5882c03
John A. Stevens <john@otherskylines.com>
2022-05-12 19:12:39 -0600

    Add created and updated times to account

--- src/backend/personalFinance/repo/account.go
+++ src/backend/personalFinance/repo/account.go
@@ -1,9 +1,13 @@
 package repo
 
+import "time"
+
 type Account struct {
-	Id          int64  `db:"id" json:"id"`
-	Name        string `db:"name" json:"name"`
-	Description string `db:"description" json:"description"`
+	Id          int64     `db:"id" json:"id"`
+	Name        string    `db:"name" json:"name"`
+	Description string    `db:"description" json:"description"`
+	CreatedAt   time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updatedAt"`
 }
 
 type CreateAccountParams struct {
@@ -34,8 +38,22 @@ func GetAccount(dao DAO, id int64) (*Account, error) {
 	defer logRepoReturn(log)
 
 	SQL := `--sql
-		SELECT * FROM account
-		WHERE account.id = $1`
+		select account.*,
+	         create_action.time as created_at,
+					 update_action.time as updated_at
+		from account
+		join event create_event on create_event.entity_id = account.id and create_event.event_type = 'create'
+		join action create_action on create_event.action_id = create_action.id
+		join lateral
+		(
+		  select action.time 
+			from event
+			join action on event.action_id = action.id
+			where event.entity_id = account.id
+			order by time desc
+			limit 1
+		) update_action on true
+		where account.id = $1`
 
 	var account Account
 	err := dao.Get(&account, SQL, id)
@@ -52,7 +70,21 @@ func GetAccounts(dao DAO) ([]*Account, error) {
 	defer logRepoReturn(log)
 
 	SQL := `--sql
-		SELECT * FROM account`
+		select account.*,
+	         create_action.time as created_at,
+					 update_action.time as updated_at
+		from account
+		join event create_event on create_event.entity_id = account.id and create_event.event_type = 'create'
+		join action create_action on create_event.action_id = create_action.id
+		join lateral
+		(
+		  select action.time 
+			from event
+			join action on event.action_id = action.id
+			where event.entity_id = account.id
+			order by time desc
+			limit 1
+		) update_action on true`
 
 	accounts := make([]*Account, 0)
 	err := dao.Select(&accounts, SQL)
