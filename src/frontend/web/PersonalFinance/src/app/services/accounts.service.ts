d655ee72398b7a928c1d2e852fa9c8194aaf5cb5
John A. Stevens <john@otherskylines.com>
2022-07-24 13:06:31 -0600

    Move common types into types file

--- src/frontend/web/PersonalFinance/src/app/services/accounts.service.ts
+++ src/frontend/web/PersonalFinance/src/app/services/accounts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, shareReplay, firstValueFrom } from 'rxjs';
+import { LoadingStatus } from './types';
 
 export  enum DebitCreditType {
   Debit = 0,
@@ -27,18 +28,11 @@ export interface Account {
   debitsAndCredits?: DebitCredit[];
 }
 
-export enum AccountLoadingStatus {
-  NotLoaded = 0,
-  Loading,
-  Loaded,
-  Error
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
-  private loadingStatus$$ = new BehaviorSubject<AccountLoadingStatus>(AccountLoadingStatus.NotLoaded);
+  private loadingStatus$$ = new BehaviorSubject<LoadingStatus>(LoadingStatus.NotLoaded);
   public loadingStatus$ = this.loadingStatus$$.pipe(shareReplay(1));
 
   private accounts$$ = new BehaviorSubject<Account[]>([]);
@@ -48,14 +42,14 @@ export class AccountsService {
 
   public async loadAccounts() {
     // TODO: handle/throttle redundant requests?
-    this.loadingStatus$$.next(AccountLoadingStatus.Loading);
+    this.loadingStatus$$.next(LoadingStatus.Loading);
 
     try {
       const accounts = await firstValueFrom(this.http.get<Account[]>('http://localhost:3000/account'))
       this.accounts$$.next(accounts);
-      this.loadingStatus$$.next(AccountLoadingStatus.Loaded);
+      this.loadingStatus$$.next(LoadingStatus.Loaded);
     } catch {
-      this.loadingStatus$$.next(AccountLoadingStatus.Error);
+      this.loadingStatus$$.next(LoadingStatus.Error);
     }
   }
 
